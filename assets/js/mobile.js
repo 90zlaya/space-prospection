f13936ad138a7fce9044d2ff29c272ab21a9f0bd
= <=>
2017-08-04 14:49:04 +0200

    Debugging mobile navigation

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -5,19 +5,25 @@ window.onload = function(){
 	mobile.setAttribute("id","mobile-navigation");
 	getNavi.parentNode.insertBefore(mobile,getNavi);
 
-	document.getElementById('mobile-navigation').onclick = function(){
+	document.getElementById('mobile-navigation').onclick = function()
+    {
 		var a = getNavi.getAttribute('style');
-		if(a){
-			getNavi.removeAttribute('style');
+		if(a)
+        {
+            getNavi.removeAttribute('style');
 			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-menu.png)';
-		} else {
+		}
+        else
+        {
 			getNavi.style.display='block';
 			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-close.png)';
 		}
 	};
 	var getElm = getNavi.getElementsByTagName("LI");
-	for(var i=0;i<getElm.length;i++){
-		if(getElm[i].children.length>1){
+	for(var i=0;i<getElm.length;i++)
+    {
+		if(getElm[i].children.length>1)
+        {
 			var smenu = document.createElement("span");
 			smenu.setAttribute("class","mobile-submenu");
 			smenu.setAttribute("OnClick","submenu("+i+")");
@@ -27,13 +33,16 @@ window.onload = function(){
 	submenu = function (i){
 		var sub = getElm[i].children[1];
 		var b = sub.getAttribute('style');
-		if(b){
+		if(b)
+        {
 			sub.removeAttribute('style');
-			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-expand.png)';
+            getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-expand.png)';
 			getElm[i].lastChild.style.backgroundColor='rgba(98, 0, 49, 0.91)';
-		} else {
+		}
+        else
+        {
 			sub.style.display='block';
-			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-collapse.png)';
+            getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-collapse.png)';
 			getElm[i].lastChild.style.backgroundColor='rgba(0, 0, 0, 0.91)';
 		}
 	};
